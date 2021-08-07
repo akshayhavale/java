71dbd6bca2ffb1d0a1616ea5fcd7c1da638ef125
akshayhavale <akshayhavale@192.168.29.98>
2021-08-07 14:58:43 +0530

    commit

--- programs-on-java/src/com/example/overloading/concept/Parent.java
+++ programs-on-java/src/com/example/overloading/concept/Parent.java
@@ -2,22 +2,6 @@ package com.example.overloading.concept;
 
 public class Parent {
 	
-	protected String name;
 	
-	public void m1() {
-		System.out.println("PARENT m1()");
-	}
-	
-	public static void m2() {
-		System.out.println("PARENT m2()");
-	}
-	
-	public int m3() {
-		return 13;
-	}
-	
-	public long m4() {
-		return 13;
-	}
 
 }
--- programs-on-java/src/com/example/overloading/concept/TestOverRiding.java
+++ programs-on-java/src/com/example/overloading/concept/TestOverRiding.java
@@ -5,24 +5,7 @@ public class TestOverRiding {
 	public static void main(String args []) {
 		
 		
-		Parent p = new Child();
 		
-	
-		/**
-		 * Normal Override
-		 */
-		p.m1();
-		
-		/**
-		 * static override
-		 */
-		
-		p.m2();
-		
-		/**
-		 * return type
-		 */
-		System.out.println(p.m3());
 		
 		
 	}
