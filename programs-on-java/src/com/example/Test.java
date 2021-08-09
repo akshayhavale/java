e836337d8b719517cf58d8ba64cdfbb83811b7b9
akshayhavale <akshayhavale@192.168.29.98>
2021-08-09 14:22:29 +0530

    commit

--- programs-on-java/src/com/example/Test.java
+++ programs-on-java/src/com/example/Test.java
@@ -4,6 +4,10 @@ public class Test {
 
 	public static void main(String args[]) {
 		System.out.println(getAccountNumber("123", "sav"));
+		
+		int a = 300;
+		byte b = (byte)a;
+		System.out.println(b);
 	}
 
 	public static String getAccountNumber(String accountNumber, String accountType) {
